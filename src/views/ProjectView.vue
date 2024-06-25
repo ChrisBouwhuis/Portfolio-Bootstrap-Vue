365ea24e55daec5c83809f0812cc02282ac7770e
Chris Bouwhuis <chrisb190203@gmail.com>
2024-06-25 12:17:28 +0200

    added a todo

---
+++ src/views/ProjectView.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+<!--  todo: write a page that gets generated based off off json data that shows off 1 project -->
+  <div class="d-flex flex-column align-items-center justify-content-center p-5">
+    <h1 class="p-2 mt-2 mb-3"># project name</h1>
+    <h2># short hand discription of the project</h2>
+  </div>
+</template>
