ddb0c6fc8526fca14502c45bdef4da047498d818
Chris Bouwhuis <chrisb190203@gmail.com>
2024-06-25 12:14:34 +0200

    added a todo

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -7,5 +7,5 @@
     <h2 class="w-50 text-center">Heb je vragen of wil je contact met mij opnemen? Dat kan! Vul het onderstaande formulier in of stuur een
       berichtje naar een van mijn socialmedia accounts. Dan ik neem zo snel mogelijk contact met je op.</h2>
   </div>
-
+<!--  todo make a nice looking form -->
 </template>
--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -5,4 +5,5 @@
     <h1 class="p-2 mt-2 mb-3">Mijn projecten</h1>
     <h2>Op deze pagina kun je een overzicht zien van de projecten die ik gemaakt heb</h2>
   </div>
+<!--  todo make a list with components that show all the projects-->
 </template>
