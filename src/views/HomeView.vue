0563e9770237cd93806d8e1e9f59e1eb9b832aa7
Chris Bouwhuis <chrisb190203@gmail.com>
2024-06-25 11:25:02 +0200

    added the first things in the introduction page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,39 +1,42 @@
-<script setup></script>
+<script setup>
+</script>
 
 <template>
-  <main class="flex flex-column justify-content-center">
-    <div>
-      <h1>Welkom op mijn portfolio website!</h1>
-      <h2>
+  <main>
+    <div class="d-flex flex-column align-items-center p-5">
+      <h1 class="p-2 mt-2 mb-3">Welkom op mijn portfolio website!</h1>
+      <h2 class="p-1 fs-4">
         Op deze website kun je meer over mij te weten komen en enkele voorbeelden zien van projecten
         die ik in het verleden heb afgerond.
       </h2>
     </div>
-    <div>
-      <img src="#" alt="Foto van mij (die ene waar ik sta)" />
-      <p>
-        <!--        Ik ben Chris Bouwhuis kom uit het-->
-        <!--        <a-->
-        <!--          href="https://www.google.com/maps/place/Boekelo,+7548+EH+Enschede/@52.2042139,6.7961489,16z/data=!3m1!4b1!4m6!3m5!1s0x47b8128b300211e1:0x77daef0fb862139e!8m2!3d52.2042146!4d6.8012987!16zL20vMGZqaHRo?authuser=0&entry=ttu"-->
-        <!--        >-->
-        <!--          mooie dorpje Boekelo.</a-->
-        <!--        >-->
-        <!--        Ik ben een student aan het-->
-        <!--        <a-->
-        <!--          href="https://www.rocvantwente.nl/voor-scholieren/mbo-opleidingen/software-developer-bol-4.html"-->
-        <!--          >ROC van Twente en volg de opleiding Software Developer.</a-->
-        <!--        >-->
-        <!--        in-->
-        <!--        <a-->
-        <!--          href="https://www.google.com/maps/place/ROC+van+Twente+-+De+Gieterij/@52.2601902,6.7860334,17z/data=!4m15!1m8!3m7!1s0x47b811fd6515954d:0x3be3aa657b041408!2sGieterij,+7553+VZ+Hengelo!3b1!8m2!3d52.2601902!4d6.7886083!16s%2Fg%2F1tl4c2xc!3m5!1s0x47b80e02905554d3:0x2862a3d5ebd1ab56!8m2!3d52.2604764!4d6.7865537!16s%2Fg%2F1tq6cky9?authuser=0&entry=ttu"-->
-        <!--        >-->
-        <!--          De Gieterij in Hengelo.-->
-        <!--        </a>-->
-        Mijn naam is Chris Bouwhuis en ik kom uit het prachtige dorp Boekelo. Ik ben student aan het
-        ROC van Twente, waar ik de versnelde opleiding Software Developer volg in De Gieterij in
-        Hengelo. In plaats van de reguliere vier jaar, rond ik deze opleiding af in drie jaar. Na
-        mijn huidige studie wil ik graag verder studeren aan het HBO binnen de softwareontwikkeling.
-      </p>
+    <div class="d-flex flex-column align-items-center">
+      <h1 class="text-decoration-underline p-2">Wie ben ik?</h1>
+      <div class="d-flex flex-column flex-xl-row-reverse justify-content-center align-items-center align-items-xl-start">
+        <p class="w-50 w-xl-50 fs-4 p-3 m-4">
+          Mijn naam is Chris Bouwhuis en ik kom uit het prachtige
+          <a
+            href="https://www.google.com/maps/place/Boekelo,+7548+EH+Enschede/@52.2042139,6.7961489,16z/data=!3m1!4b1!4m6!3m5!1s0x47b8128b300211e1:0x77daef0fb862139e!8m2!3d52.2042146!4d6.8012987!16zL20vMGZqaHRo?authuser=0&entry=ttu"
+          >
+            dorp Boekelo</a
+          >. Ik ben student aan het
+          <a
+            href="https://www.rocvantwente.nl/voor-scholieren/mbo-opleidingen/software-developer-bol-4.html"
+          >
+            ROC van Twente</a
+          >, waar ik de versnelde opleiding Software Developer volg in
+          <a
+            href="https://www.google.com/maps/place/ROC+van+Twente+-+De+Gieterij/@52.2601902,6.7860334,17z/data=!4m15!1m8!3m7!1s0x47b811fd6515954d:0x3be3aa657b041408!2sGieterij,+7553+VZ+Hengelo!3b1!8m2!3d52.2601902!4d6.7886083!16s%2Fg%2F1tl4c2xc!3m5!1s0x47b80e02905554d3:0x2862a3d5ebd1ab56!8m2!3d52.2604764!4d6.7865537!16s%2Fg%2F1tq6cky9?authuser=0&entry=ttu"
+          >De Gieterij in Hengelo</a
+          >. In plaats van de reguliere vier jaar, rond ik deze opleiding af in drie jaar. Na mijn
+          huidige studie wil ik graag verder studeren aan het HBO binnen de softwareontwikkeling.
+        </p>
+        <img
+          class="w-50 w-xl-25 m-5 rounded border border-primary border-3 shadow"
+          src="../../public/img/ik-staand-smaller.jpg"
+          alt="een foto waar sta"
+        />
+      </div>
     </div>
   </main>
 </template>
