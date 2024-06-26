8b1d7c913475fac2db19490806b1999f223d1918
Chris Bouwhuis <chrisb190203@gmail.com>
2024-06-26 14:04:33 +0200

    vanaf dit punt ga ik hier aan werken als zij project voor mijzelf

---
+++ src/components/submit-button.vue
@@ -0,0 +1,19 @@
+<script setup></script>
+
+<template>
+  <button type="submit" class="btn btn-primary hoverEffect m-1">
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped>
+.hoverEffect {
+  transition: all 0.3s;
+}
+
+.hoverEffect:hover {
+  transform: scale(1.1);
+  background: linear-gradient(45deg, hsla(112, 85%, 52%, 0.5), hsla(198, 88%, 45%, 0.5) 100%);
+  color: black;
+}
+</style>
--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,13 +1,48 @@
-<script setup></script>
+<script setup>
+import SubmitButton from '@/components/submit-button.vue'
+</script>
 
 <template>
-  <div class="d-flex flex-column justify-content-center align-items-center">
-    <h1 class="p-5">Contact</h1>
-    <h2 class="w-50 text-center">
-      Heb je vragen of wil je contact met mij opnemen? Dat kan! Vul het onderstaande formulier in of
-      stuur een berichtje naar een van mijn socialmedia accounts. Dan ik neem zo snel mogelijk
-      contact met je op.
-    </h2>
+  <div class="container">
+    <div class="d-flex flex-column justify-content-center align-items-center">
+      <h1 class="mt-5 mb-3">Contact</h1>
+      <h2 class="text-center fs-3">
+        Heb je vragen of wil je contact met mij opnemen? Dat kan!
+        <span class="fw-bold">Vul het onderstaande formulier in</span> of stuur een berichtje naar
+        een van mijn socialmedia accounts. Dan ik neem zo snel mogelijk contact met je op.
+      </h2>
+    </div>
+    <form class="w-75 mx-auto mt-5">
+      <div class="mb-3">
+        <label for="name" class="form-label">Naam</label>
+        <input
+          type="text"
+          class="form-control"
+          id="name"
+          placeholder="Vul hier je naam in"
+          aria-describedby="nameHelp"
+        />
+      </div>
+      <div class="mb-3">
+        <label for="email" class="form-label">Email</label>
+        <input
+          type="email"
+          class="form-control"
+          id="email"
+          placeholder="Vul hier je Email in"
+          aria-describedby="emailHelp"
+        />
+      </div>
+      <div class="mb-3">
+        <label for="message" class="form-label">Bericht</label>
+        <textarea
+          class="form-control"
+          id="message"
+          placeholder="Plaats hier je bericht"
+          rows="3"
+        ></textarea>
+      </div>
+      <submit-button>Verstuur</submit-button>
+    </form>
   </div>
-  <!--  todo make a nice looking form -->
 </template>
